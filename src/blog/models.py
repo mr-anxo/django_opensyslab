9ac36ccda6c052fa9a2ac7ff76a117cffe80c4cf
Axel Ahin <mail.ahinaxel@gmail.com>
2024-06-09 23:54:50 +0000

    --- Auto Git Commit ---

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField() # Nom transformé à utiliser dans les url
+    published = models.BooleanField(default=False)
+    date = models.DateField(blank=True)
+    content = models.TextField()
+    description = models.TextField()
+    
