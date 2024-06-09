db0edd873581eaf98c8afce310c27a5e9230f895
Axel Ahin <mail.ahinaxel@gmail.com>
2024-06-09 23:24:42 +0000

    --- Auto Git Commit ---

--- src/opensyslab/settings.py
+++ src/opensyslab/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS = []
 
 # Default primary key field type
