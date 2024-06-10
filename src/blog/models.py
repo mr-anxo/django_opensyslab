76e16ad56700447642cddb6ab16d6f9e34960812
Axel Ahin <mail.ahinaxel@gmail.com>
2024-06-10 00:24:57 +0000

    --- Auto Git Commit ---

--- src/blog/models.py
+++ src/blog/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField() # Nom transformé à utiliser dans les url
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
     
